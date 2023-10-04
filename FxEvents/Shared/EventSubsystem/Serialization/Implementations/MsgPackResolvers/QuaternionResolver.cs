f74c9f7faee9534a90193634b796c4b2456b1c5b
Leonardo Emanuele <manups4e@gmail.com>
2023-10-04 21:05:02 +0200

    enhanced Vectors and Quaternion serialization Resolver

--- FxEvents/Shared/EventSubsystem/Serialization/Implementations/MsgPackResolvers/QuaternionResolver.cs
+++ FxEvents/Shared/EventSubsystem/Serialization/Implementations/MsgPackResolvers/QuaternionResolver.cs
@@ -5,18 +5,47 @@ namespace FxEvents.Shared.Serialization.Implementations.MsgPackResolvers
 {
     public class QuaternionResolver : MessagePackSerializer<Quaternion>
     {
+        private readonly MessagePackSerializer<float> _itemSerializer;
         public QuaternionResolver(MsgPack.Serialization.SerializationContext ownerContext) : base(ownerContext)
         {
+            this._itemSerializer = ownerContext.GetSerializer<float>();
         }
 
         protected override void PackToCore(Packer packer, Quaternion objectTree)
         {
-            packer.Pack(objectTree.ToArray());
+            packer.PackArrayHeader(4);
+            _itemSerializer.PackTo(packer, objectTree.X);
+            _itemSerializer.PackTo(packer, objectTree.Y);
+            _itemSerializer.PackTo(packer, objectTree.Z);
+            _itemSerializer.PackTo(packer, objectTree.W);
         }
 
         protected override Quaternion UnpackFromCore(Unpacker unpacker)
         {
-            return new Quaternion((float[])unpacker.LastReadData.ToObject());
+            float[] values = new float[4];
+            for (int i = 0; i < 4; i++)
+            {
+                if (!unpacker.Read())
+                {
+                    throw SerializationExceptions.NewMissingItem(i);
+                }
+
+                float item;
+                if (!unpacker.IsArrayHeader && !unpacker.IsMapHeader)
+                {
+                    item = this._itemSerializer.UnpackFrom(unpacker);
+                }
+                else
+                {
+                    using (Unpacker subtreeUnpacker = unpacker.ReadSubtree())
+                    {
+                        item = this._itemSerializer.UnpackFrom(subtreeUnpacker);
+                    }
+                }
+
+                values[i] = item;
+            }
+            return new Quaternion(values[0], values[1], values[2], values[3]);
         }
 
     }
