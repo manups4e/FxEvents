45773a12e9b45e99adf821d83af1d899713b9a5c
Leonardo Emanuele <manups4e@gmail.com>
2023-11-05 10:47:36 +0100

    BynaryHelper: don't return null when converting from string to byte[]

--- FxEvents/Shared/BinaryHelper.cs
+++ FxEvents/Shared/BinaryHelper.cs
@@ -19,7 +19,8 @@ namespace FxEvents.Shared
         public static byte[] StringToBytes(this string str)
         {
             char[] arr = str.ToCharArray();
-            if (arr[2] != '-' && arr[5] != '-') return default;
+            if (arr[2] != '-' && arr[5] != '-')
+                return Enumerable.Range(0, str.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(str.Substring(x, 2), 16)).ToArray();
             return str.Split('-').Select(x => byte.Parse(x, NumberStyles.HexNumber)).ToArray();
         }
 
