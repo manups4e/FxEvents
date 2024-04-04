5ef398c9b5eeabeb86349e2128e3f303a418c691
Leonardo Emanuele <manups4e@gmail.com>
2024-04-04 11:09:34 +0200

    renamed FxEvents/FXEvents => FxEvents/src

--- FxEvents/scripts/post_client.bat
+++
@@ -1,3 +0,0 @@
-echo Current working directory is: %cd%
-xcopy /Y /E "..\CompiledLibs\Client\Debug FiveM\net452\FxEvents.Client.dll" "L:\FiveM\FreeRoamProject\resources\[local]\frp\Client"
-echo Done!
