a3c85231b02be02c9e9f4cc593cb0ddf113ae2e0
Leonardo Emanuele <manups4e@gmail.com>
2024-06-13 18:06:54 +0200

    Added Fivem-related resolvers

---
+++ src/FxEvents.Shared/EventSubsystem/Serialization/Implementations/MsgPackResolvers/EntityResolver.cs
@@ -0,0 +1,76 @@
+﻿using MsgPack;
+using MsgPack.Serialization;
+
+namespace FxEvents.Shared.EventSubsystem.Serialization.Implementations.MsgPackResolvers
+{
+    /// <summary>
+    /// For this one and all its derivates.. we use NetworkID to keep consistency between each side.
+    /// </summary>
+    public class EntityResolver : MessagePackSerializer<Entity>
+    {
+        public EntityResolver(MsgPack.Serialization.SerializationContext ownerContext) : base(ownerContext)
+        {
+        }
+
+        protected override void PackToCore(Packer packer, Entity objectTree)
+        {
+            packer.Pack(objectTree.NetworkId);
+        }
+
+        protected override Entity UnpackFromCore(Unpacker unpacker)
+        {
+            return Entity.FromNetworkId(unpacker.LastReadData.AsInt32());
+        }
+    }
+
+    public class PedResolver : MessagePackSerializer<Ped>
+    {
+        public PedResolver(MsgPack.Serialization.SerializationContext ownerContext) : base(ownerContext)
+        {
+        }
+
+        protected override void PackToCore(Packer packer, Ped objectTree)
+        {
+            packer.Pack(objectTree.NetworkId);
+        }
+
+        protected override Ped UnpackFromCore(Unpacker unpacker)
+        {
+            return (Ped)Entity.FromNetworkId(unpacker.LastReadData.AsInt32());
+        }
+    }
+
+    public class VehicleResolver : MessagePackSerializer<Vehicle>
+    {
+        public VehicleResolver(MsgPack.Serialization.SerializationContext ownerContext) : base(ownerContext)
+        {
+        }
+
+        protected override void PackToCore(Packer packer, Vehicle objectTree)
+        {
+            packer.Pack(objectTree.NetworkId);
+        }
+
+        protected override Vehicle UnpackFromCore(Unpacker unpacker)
+        {
+            return (Vehicle)Entity.FromNetworkId(unpacker.LastReadData.AsInt32());
+        }
+    }
+
+    public class PropResolver : MessagePackSerializer<Prop>
+    {
+        public PropResolver(SerializationContext ownerContext) : base(ownerContext)
+        {
+        }
+
+        protected override void PackToCore(Packer packer, Prop objectTree)
+        {
+            packer.Pack(objectTree.NetworkId);
+        }
+
+        protected override Prop UnpackFromCore(Unpacker unpacker)
+        {
+            return (Prop)Entity.FromNetworkId(unpacker.LastReadData.AsInt32());
+        }
+    }    
+}
---
+++ src/FxEvents.Shared/EventSubsystem/Serialization/Implementations/MsgPackResolvers/PlayerResolver.cs
@@ -0,0 +1,27 @@
+﻿using MsgPack;
+using MsgPack.Serialization;
+using System;
+
+namespace FxEvents.Shared.EventSubsystem.Serialization.Implementations.MsgPackResolvers
+{
+    public class PlayerResolver : MessagePackSerializer<Player>
+    {
+        public PlayerResolver(SerializationContext ownerContext) : base(ownerContext)
+        {
+        }
+
+        protected override void PackToCore(Packer packer, Player objectTree)
+        {
+#if CLIENT
+            packer.Pack(objectTree.ServerId);
+#elif SERVER
+            packer.Pack(int.Parse(objectTree.Handle));
+#endif
+        }
+
+        protected override Player UnpackFromCore(Unpacker unpacker)
+        {
+            return EventHub.Instance.GetPlayers[unpacker.LastReadData.AsInt32()];
+        }
+    }
+}
