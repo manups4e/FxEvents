9a44bdc6d61786205796ccd06c8a15eb94ba01bf
Leonardo Emanuele <manups4e@gmail.com>
2024-06-26 20:52:23 +0200

    Ignore and Force attributes are a no go with this msgpack.. shame!

--- src/FxEvents.Shared/EventSubsystem/Attributes/ForceAttribute.cs
+++ src/FxEvents.Shared/EventSubsystem/Attributes/ForceAttribute.cs
@@ -5,6 +5,7 @@ namespace FxEvents.Shared.Attributes
     /// <summary>
     /// Indicates that this property should be forcefully added to serialization.
     /// </summary>
+    [Obsolete("the current MsgPack version does not consent to serialize non public members/fields")]
     public class ForceAttribute : Attribute
     {
     }
--- src/FxEvents.Shared/EventSubsystem/Attributes/IgnoreAttribute.cs
+++ src/FxEvents.Shared/EventSubsystem/Attributes/IgnoreAttribute.cs
@@ -5,6 +5,7 @@ namespace FxEvents.Shared.Attributes
     /// <summary>
     /// Indicates that this property should be disregarded from serialization.
     /// </summary>
+    [Obsolete("the current MsgPack version does not consent to ignore non public members/fields")]
     public class IgnoreAttribute : Attribute
     {
     }
