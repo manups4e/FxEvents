f4c38918f1ed89c2560c2ce0a6517fc1d06a6918
Leonardo Emanuele <manups4e@gmail.com>
2024-07-06 23:18:49 +0200

    remove warning for get internal

--- src/FxEvents.Shared/EventSubsystem/BaseGateway.cs
+++ src/FxEvents.Shared/EventSubsystem/BaseGateway.cs
@@ -447,15 +447,6 @@ namespace FxEvents.Shared.EventSubsystem
 
         protected async Task<T> GetInternal<T>(int source, string endpoint, Binding binding, params object[] args)
         {
-            if (EventHub.Gateway.GetSecret(source).Length == 0)
-            {
-#if SERVER
-                Logger.Warning($"Cannot send {endpoint} event to player {API.GetPlayerName("" + source)} they're still connecting or script is not loaded yet on their client");
-#elif CLIENT
-                Logger.Warning($"Cannot send {endpoint} event to server script is not loaded yet");
-#endif
-                return default;
-            }
             StopwatchUtil stopwatch = StopwatchUtil.StartNew();
             EventMessage message = await CreateAndSendAsync(EventFlowType.Circular, source, endpoint, binding, args);
             EventValueHolder<T> holder = new EventValueHolder<T>();
